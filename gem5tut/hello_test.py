70d85412e3dcf94743048b049835eeb4c6a45723
Eli Baum <eli.baum@yale.edu>
2019-02-03 17:55:49 -0500

    adding new simobjects

---
+++ gem5tut/hello_test.py
@@ -0,0 +1,10 @@
+import m5
+from m5.objects import *
+
+root = Root(full_system = False)
+root.hello = HelloObject()
+
+m5.instantiate()
+print("begin")
+exit_event = m5.simulate()
+print('exit @ tick {} because {}'.format(m5.curTick(), exit_event.getCause()))
