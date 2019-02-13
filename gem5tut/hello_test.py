3901fbd6fa31c033578e1d8b599883454c8481f3
Eli Baum <eli.baum@yale.edu>
2019-02-13 01:24:21 -0500

    simple memobj

--- gem5tut/hello_test.py
+++ gem5tut/hello_test.py
@@ -2,7 +2,9 @@ import m5
 from m5.objects import *
 
 root = Root(full_system = False)
-root.hello = HelloObject()
+
+root.hello = HelloObject(time_to_wait = '2us', number_of_fires = 10)
+root.hello.goodbye_object = GoodbyeObject(buffer_size='100B')
 
 m5.instantiate()
 print("begin")
