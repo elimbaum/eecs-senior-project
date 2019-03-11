90c71bbb261f1ea5d538373c483eac8edcf43232
emb99 <eli.baum@yale.edu>
2019-03-11 12:27:18 -0400

    linked-function modification is WORKING!

--- kaleidoscope/libkld.cpp
+++ kaleidoscope/libkld.cpp
@@ -1,6 +1,12 @@
 #include <cstdio>
+#include <cmath>
 
 extern "C" double printd2(double X) {
   fprintf(stderr, "(3: %f)\n", X);
   return X;
+}
+
+extern "C" double _hyp(double x, double y) {
+  fprintf(stderr, "computing hyp %f, %f == %f?\n", x, y, hypot(x, y));
+  return 6;
 }
--- passes/test/test.cpp
+++ passes/test/test.cpp
@@ -3,6 +3,7 @@
 #include "llvm/IR/LegacyPassManager.h"
 #include "llvm/IR/IRBuilder.h"
 #include "llvm/IR/InstrTypes.h"
+#include "llvm/IR/Module.h"
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/Transforms/IPO/PassManagerBuilder.h"
 #include "llvm/Transforms/Utils/BasicBlockUtils.h"
@@ -20,24 +21,44 @@ namespace {
         return false;
 
       errs() << "Mutating!\n";
+      
+      LLVMContext& Ctx = F.getContext();
+      std::vector<Type *> paramTypes = {Type::getDoubleTy(Ctx), Type::getDoubleTy(Ctx)};
+      Type * retType = Type::getDoubleTy(Ctx);
 
+      FunctionType * _funcType = FunctionType::get(retType, paramTypes, false);
+      FunctionCallee _func = F.getParent()->getOrInsertFunction("_hyp", _funcType);
+      
+      IRBuilder<> builder(& F.front());
+      auto A = F.args().begin();
+      // A->print(llvm::errs());
+      // builder.CreateCall(_func, {A, ++A});
       for (auto &B : F) { // loop through BBs
         for (auto &I : B) {
-          if (auto *op = dyn_cast<BinaryOperator>(&I)) {
-            IRBuilder<> builder(op);
+          if (auto *ret = dyn_cast<ReturnInst>(&I)) {
+            // IRBuilder<> builder(op);
 
-            Value *lhs = op->getOperand(0);
-            Value *rhs = op->getOperand(1);
-            Value *mul = builder.CreateFAdd(lhs, rhs);
+            // Value * a = op->getOperand(0);
+            // Value * b = op->getOperand(1);
+            // Value * args[] = {a, b};
 
-            // replace operand
-            for (auto& U : op->uses()) {
-              User * user = U.getUser();
-              user->setOperand(U.getOperandNo(), mul);
-            }
+            // builder.CreateCall(_func, args);
+            // return true;
+            errs() << "Found return inst: ";
+            ret->print(llvm::errs());
+            errs() << "\n";
+
+            IRBuilder<> builder(ret);
+            Value * f = builder.CreateCall(_func, {A, ++A});
+            auto * r = builder.CreateRet(f);
+            errs() << "New: "; F.print(llvm::errs()); errs() << "\n";
+            ret->eraseFromParent();
+            
             return true;
           }
         }
+        // This BB did not contain the return.
+        B.removeFromParent();
       }
 
       return false;
@@ -48,9 +69,14 @@ namespace {
 
 char TestPass::ID = 0;
 
-// Example code specifies auto-registration below that I think only applies
+// Example code specifies auto-registration here that I think only applies
 // if I wanted to apply this pass to my compiled code, instead of the JIT.
 
 FunctionPass * llvm::createTestPass() {
   return new TestPass();
 }
+
+extern "C" double pi(void) {
+  return 3;
+}
+
